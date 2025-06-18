a213556ebd9134e3b22634dff27f4a280206c082
Jeff Uchitjil <git@k9kld.org>
2025-06-18 18:33:44 -0500

    fix: Added logic
    wip: Some work in progress
    
    docs: Added some docs

--- BuildTestConsoleApp/Program.cs
+++ BuildTestConsoleApp/Program.cs
@@ -8,5 +8,6 @@ Console.WriteLine("Hello, World fix");
 
 // Add a new comment
 // Another comment
+// More stuff
 Version version = Assembly.GetExecutingAssembly().GetName().Version ?? Version.Parse("0.0.0.0");
 Console.WriteLine($"The version of the currently executing assembly is: {version}");
