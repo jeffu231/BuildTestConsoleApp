396f48ab657b71ca9a159f3e38d650a7ae341fc8
Jeff Uchitjil <git@k9kld.org>
2025-06-18 18:26:05 -0500

    docs: Add a new comment
    * fix: Fixed an unknown bug.
    * log: Added some new logging.
    * wip: Still working on figuring this out.
    * test: Looking at some testing code.

--- BuildTestConsoleApp/Program.cs
+++ BuildTestConsoleApp/Program.cs
@@ -7,5 +7,6 @@ Console.WriteLine("Hello, World");
 Console.WriteLine("Hello, World fix");
 
 // Add a new comment
+// Another comment
 Version version = Assembly.GetExecutingAssembly().GetName().Version ?? Version.Parse("0.0.0.0");
 Console.WriteLine($"The version of the currently executing assembly is: {version}");
